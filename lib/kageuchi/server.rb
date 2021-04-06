0d1550cc855d1537588abcfd5f7a3cc7adfa63d8
s4na <appletea.umauma@gmail.com>
2021-04-06 21:43:51 +0900

    Separate start methods

--- lib/kageuchi/server.rb
+++ lib/kageuchi/server.rb
@@ -4,7 +4,7 @@ require "socket"
 
 module Kageuchi
   class Server
-    attr_reader :status, :logs
+    attr_reader :status, :logs, :request
 
     def initialize(host = "localhost", port = 3000)
       @host = host
@@ -17,38 +17,54 @@ module Kageuchi
       @server = TCPServer.open(@host, @port)
 
       loop do
-        socket = @server.accept
-        request_params = socket.gets.chomp.match(/^(?<verb>[A-Z]*) (?<path>[^ ]*) (?<ver>.*)$/)
-        if request_params
-          headers = []
-          while line = socket.gets.chomp
-            break if line.bytesize.zero?
-
-            headers << line.split(": ")
-          end
-          request = {
-            VERB: request_params[:verb],
-            PATH: request_params[:path],
-            VERSION: request_params[:ver],
-            HEDERS: headers
-          }
-          pp request
-          @logs << request
-        end
-
+        @socket = @server.accept
         @status = :running
-        next unless line.bytesize.zero?
 
-        socket.write "HTTP/1.1 200 OK\r\n"
-        socket.write "\r\n"
-        socket.write "Hello. This is Kageuchi server\r\n"
-        socket.close
+        next unless handle
+
+        request_close
       end
     end
 
     def close
       @server.close
       @status = :closed
+    end
+
+    private
+
+    def handle
+      request_params = @socket.gets.chomp.match(/^(?<verb>[A-Z]*) (?<path>[^ ]*) (?<ver>.*)$/)
+      if request_params
+        headers, line = set_header
+        request = {
+          VERB: request_params[:verb],
+          PATH: request_params[:path],
+          VERSION: request_params[:ver],
+          HEDERS: headers
+        }
+        pp request
+        @logs << request
+      end
+
+      line.bytesize.zero?
+    end
+
+    def set_header
+      headers = []
+      while line = @socket.gets.chomp
+        break if line.bytesize.zero?
+
+        headers << line.split(": ")
+      end
+      [headers, line]
+    end
+
+    def request_close
+      @socket.write "HTTP/1.1 200 OK\r\n"
+      @socket.write "\r\n"
+      @socket.write "Hello. This is Kageuchi server\r\n"
+      @socket.close
     end
   end
 end
